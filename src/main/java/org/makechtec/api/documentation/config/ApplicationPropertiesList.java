2a6cc615d16100c858a462696b1cc4b591219b67
Angel Dominguez <angeldomp49@gmail.com>
2023-04-17 13:33:22 -0600

    configured for external application.properties, also profiles, application-dev.properties not included in repo.

--- src/main/java/org/makechtec/api/documentation/config/ApplicationPropertiesList.java
+++ src/main/java/org/makechtec/api/documentation/config/ApplicationPropertiesList.java
@@ -8,33 +8,33 @@ import org.springframework.beans.factory.annotation.Value;
 @Setter
 public class ApplicationPropertiesList {
 
-    @Value("server.servlet.context-path")
+    @Value("${server.servlet.context-path}")
     private String contextPath;
 
-    @Value("db_user")
+    @Value("${db_user}")
     private String dbUser;
-    @Value("db_password")
+    @Value("${db_password}")
     private String dbPassword;
 
-    @Value("db_host")
+    @Value("${db_host}")
     private String dbHost;
 
-    @Value("db_port")
+    @Value("${db_port}")
     private String dbPort;
 
-    @Value("db_name")
+    @Value("${db_name}")
     private String dbName;
 
-    @Value("cors_client_host")
+    @Value("${cors_client_host}")
     private String corsClientHost;
 
-    @Value("cors_client_port")
+    @Value("${cors_client_port}")
     private String corsClientPort;
 
-    @Value("cors_protocol")
+    @Value("${cors_protocol}")
     private String corsProtocol;
 
-    @Value("cors_methods")
+    @Value("${cors_methods}")
     private String corsMethods;
 
 }
